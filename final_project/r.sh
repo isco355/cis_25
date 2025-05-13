787d61c1c44455c1fdd6363ef751185e6462e1f7
Francisco Ortega <ortega.fo53@gmail.com>
2025-05-12 21:20:44 -0700

    remove comments

--- final_project/r.sh
+++ final_project/r.sh
@@ -1,13 +1,6 @@
 clear 
 
 echo "compiling source code"
-# g++ single.cpp  -o main -std=c++20 && ./main
 
-# g++ src/code/main.cpp  src/code/route.cpp src/code/flyManager.cpp src/code/graphLoader.cpp  src/code/dijkstra.cpp  src/code/interface.cpp -o src/compile/main -std=c++20 && ./src/compile/main
 g++ src/code/*.cpp -o src/compile/main -std=c++20  && ./src/compile/main
 
-#generate image
-# dot -Tjpeg -Gdip=600 dots/default.dot -o render/routes.jpg
-#
-# dot -Tjpeg -Gdip=600 dots/track_route.dot -o render/short_route_mark.jpg
-#
